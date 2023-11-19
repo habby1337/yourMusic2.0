9d29b442ea3fea0edb88f993e580b7bfc0db28b4
Federico Tensi <federicotensi@gmail.com>
2023-11-19 20:29:21 +0100

    basic card slider

--- frontend/src/ui/Discover.tsx
+++ frontend/src/ui/Discover.tsx
@@ -1,8 +1,64 @@
+import { ArrowRight } from "lucide-react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+const Title = () => {
+	return (
+		<div className=" flex justify-between items-baseline">
+			<h1 className="text-[28px]">Discover new</h1>
+			<div className="flex align-bottom items-center gap-1">
+				<p className="text-sm ">View more genres</p>
+				<ArrowRight size={14} strokeWidth={3} />
+			</div>
+		</div>
+	);
+};
+
+const CardSlider = () => {
+	return (
+		<div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start gap-5">
+			<GenreCard
+				title="Blues"
+				description="Listen to popular tracks"
+				imageUrl="https://seeded-session-images.scdn.co/v1/img/artist/6iWF9SfkRbKQwIHaTzgpgX/de"
+			/>
+			<GenreCard
+				title="Hip Hop"
+				description="Get some hiphop vibes"
+				imageUrl="https://seeded-session-images.scdn.co/v1/img/artist/45rVykSnbTDeLDAcguWln3/en"
+			/>
+			<GenreCard
+				title="Hip Hop"
+				description="Get some hiphop vibes"
+				imageUrl="https://seeded-session-images.scdn.co/v1/img/artist/45rVykSnbTDeLDAcguWln3/en"
+			/>
+			<GenreCard
+				title="Hip Hop"
+				description="Get some hiphop vibes"
+				imageUrl="https://seeded-session-images.scdn.co/v1/img/artist/45rVykSnbTDeLDAcguWln3/en"
+			/>
+		</div>
+	);
+};
+
+const GenreCard = ({ title, description, imageUrl }: { title: string; description: string; imageUrl: string }) => {
+	return (
+		<Card className="w-3/6 h-56 relative overflow-clip border-0 bg-neutral-800 rounded-3xl flex-none mb-8">
+			<div className="absolute flex radial-bg  backdrop-blur-[1px] text-white w-full h-48 font-bold justify-center items-center">
+				<div className="text-lg   ">{title}</div>
+			</div>
+			<img src={imageUrl} alt="" className="w-full h-48" />
+			<div className="flex justify-center ">
+				<CardDescription className="text-white font-bold text-base pt-1">{description}</CardDescription>
+			</div>
+		</Card>
+	);
+};
+
 const Discover = () => {
 	return (
-		<div className="mt-8 flex justify-between items-baseline">
-			<h1 className="text-[28px]">Discover new</h1>
-			<p className="text-sm ">View more genres</p>
+		<div className="mt-8">
+			<Title />
+			<CardSlider />
 		</div>
 	);
 };
