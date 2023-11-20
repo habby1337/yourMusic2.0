f8fdc10aac9d434ee267bfbefdbb3441c9dbc13a
Federico Tensi <federicotensi@gmail.com>
2023-11-20 13:36:04 +0100

    infinite scroll with button

--- frontend/src/ui/Suggestions.tsx
+++ frontend/src/ui/Suggestions.tsx
@@ -1,13 +1,57 @@
 import { trackList } from "@/helpers/arrayList";
 import { ResultTrackItem } from "./Search";
 import { track } from "@/helpers/types";
+import { useInfiniteQuery } from "react-query";
+import { generateRandomString } from "@/helpers/utils";
+import { API_URL } from "@/helpers/endpoints";
 
 const Suggestions = () => {
+	const fetchSuggestions = async ({ pageParam = 1 }: { pageParam?: number }) => {
+		const randomString = generateRandomString(3);
+
+		const response = await fetch(
+			`${API_URL}/getSearchInfo.php?q=${randomString}&limit=5&offset=${(pageParam - 1) * 5}`,
+		);
+
+		const data = await response.json();
+
+		const suggestions = data.tracks.items.map((item: track) => ({
+			id: item.id,
+			name: item.name,
+			album: {
+				id: item.album.id,
+				name: item.album.name,
+				images: item.album.images,
+			},
+			artists: item.artists.map((artist: any) => ({
+				id: artist.id,
+				name: artist.name,
+				images: artist.images,
+			})),
+		}));
+
+		return {
+			suggestions,
+			nextPage: data.tracks.next,
+		};
+	};
+
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery("suggestions", fetchSuggestions, {
+		getNextPageParam: (lastPage) => lastPage.nextPage || null,
+	});
+
+	const allSuggestions = data ? data.pages.flatMap((page) => page.suggestions) : [];
+
 	return (
 		<div>
-			<h4 className="mb-4 font-semibold">Suggestions for you</h4>
+			<h4 className="mb-4 mt-4 font-semibold">Suggestions for you</h4>
 			<div className="max-h-[30vh] overflow-scroll">
-				<SuggestionCards />
+				<SuggestionCards suggestions={allSuggestions} />
+				{hasNextPage && (
+					<button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+						{isFetchingNextPage ? "Loading more..." : "Load More"}
+					</button>
+				)}
 			</div>
 		</div>
 	);
@@ -15,10 +59,10 @@ const Suggestions = () => {
 
 export default Suggestions;
 
-const SuggestionCards = () => {
+const SuggestionCards = ({ suggestions }: { suggestions: track[] }) => {
 	// get 20 suggestions from tracklist starting at random index
-	const randomIndex = Math.floor(Math.random() * trackList.length);
-	const suggestions = trackList.slice(randomIndex, randomIndex + 5);
+	// const randomIndex = Math.floor(Math.random() * trackList.length);
+	// const suggestions = trackList.slice(randomIndex, randomIndex + 5);
 
 	return (
 		<>
