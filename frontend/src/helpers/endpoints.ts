54059630e83084de81893a1436bb89c78d1dab27
Federico Tensi <federicotensi@gmail.com>
2023-11-22 20:57:15 +0100

    viteconfig to enable process.env

--- frontend/src/helpers/endpoints.ts
+++ frontend/src/helpers/endpoints.ts
@@ -1,2 +1,7 @@
 export const API_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL;
+// Create a type definition file for the environment variables:
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// export const API_URL = window["env"]["BACKEND_API_URL"];
 console.log(API_URL, import.meta.env, process.env);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+// export default defineConfig({
+// 	plugins: [react()],
+// 	resolve: {
+// 		alias: {
+// 			"@": path.resolve(__dirname, "./src"),
+// 		},
+// 	},
+// 	build: {
+// 		emptyOutDir: true,
+// 	},
+// });
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
-	build: {
-		rollupOptions: {
-			plugins: [],
+		define: {
+			"process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
 		},
-	},
+	};
 });
