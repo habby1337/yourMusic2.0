699f04c8b3f27d327a430ac134b8fdce7024b2e5
Federico Tensi <federicotensi@gmail.com>
2023-11-23 18:11:08 +0100

    cors env file test

--- backend/api/v1/cors_middleware.php
+++ backend/api/v1/cors_middleware.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: ". getenv("cors_url"));
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 ?>
