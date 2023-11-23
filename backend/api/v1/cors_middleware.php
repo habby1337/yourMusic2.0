b7d0bb36938b3c8c3b463d8537dee3c85977b71b
Federico Tensi <federicotensi@gmail.com>
2023-11-23 15:46:26 +0100

    changed to all

--- backend/api/v1/cors_middleware.php
+++ backend/api/v1/cors_middleware.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://sp.tensi.dev");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 ?>
