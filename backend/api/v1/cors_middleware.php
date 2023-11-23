54e2428e820ce1415011c0accae4da335afeee5a
Federico Tensi <federicotensi@gmail.com>
2023-11-23 15:38:49 +0100

    testing cors

---
+++ backend/api/v1/cors_middleware.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: https://sp.tensi.dev");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+?>
