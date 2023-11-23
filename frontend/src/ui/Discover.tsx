9918fada1acd667c172860a71d8089e49c74f5b9
Federico Tensi <federicotensi@gmail.com>
2023-11-23 19:00:44 +0100

    fixed discover css

--- frontend/src/ui/Discover.tsx
+++ frontend/src/ui/Discover.tsx
@@ -6,9 +6,9 @@ import { handleAddToQueue } from "@/helpers/utils";
 
 const Title = () => {
 	return (
-		<div className=" flex justify-between items-baseline mb-4">
+		<div className="flex items-baseline justify-between mb-4 ">
 			<h1 className="text-[28px]">Discover new</h1>
-			<div className="flex align-bottom items-center gap-1">
+			<div className="flex items-center gap-1 align-bottom">
 				<p className="text-sm ">View more genres</p>
 				<ArrowRight size={14} strokeWidth={3} />
 			</div>
@@ -18,7 +18,7 @@ const Title = () => {
 
 const CardSlider = () => {
 	return (
-		<div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start gap-5">
+		<div className="flex flex-no-wrap items-start gap-5 overflow-x-scroll scrolling-touch">
 			{genres.slice(0, 10).map((genre) => (
 				<GenreCard
 					title={genre.title}
@@ -36,14 +36,14 @@ const GenreCard = ({ title, description, imageUrl, trackUri }: genre) => {
 	return (
 		<Card
 			onClick={() => handleAddToQueue(trackUri)}
-			className="cursor-pointer w-3/6 lg:w-1/6 h-[15rem] relative overflow-clip border-0 bg-neutral-800 rounded-3xl flex-none mb-8"
+			className="cursor-pointer w-3/6 sm:w-1/4 md:w-1/5 lg:w-1/6 h-[15rem] relative overflow-clip border-0 bg-neutral-800 rounded-3xl flex-none mb-8"
 		>
 			<div className="absolute flex radial-bg  backdrop-blur-[1px] text-white w-full h-48 font-bold justify-center items-center rounded-b-3xl">
-				<div className="text-lg   ">{title}</div>
+				<div className="text-lg ">{title}</div>
 			</div>
 			<img src={imageUrl} alt="" className="w-full h-48 rounded-b-3xl" />
-			<div className="  p-3 pt-0">
-				<CardDescription className="text-white text-sm pt-1">{description}</CardDescription>
+			<div className="p-3 pt-0 ">
+				<CardDescription className="pt-1 text-sm text-white">{description}</CardDescription>
 			</div>
 		</Card>
 	);
