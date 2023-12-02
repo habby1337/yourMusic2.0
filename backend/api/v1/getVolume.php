c5b2ce59b71f9ac83b6c4ee234560ead35a665d0
Federico Tensi <federicotensi@gmail.com>
2023-12-02 15:45:19 +0100

    Create getVolume.php

---
+++ backend/api/v1/getVolume.php
@@ -0,0 +1,35 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
+
+include 'cors_middleware.php';
+
+$session = new SpotifyWebAPI\Session(
+    getenv("spotify_client_id"),
+    getenv("spotify_client_secret")
+);
+
+$accessToken = unserialize(file_get_contents("data/.accesstoken"));
+$refreshToken = unserialize(file_get_contents("data/.refreshToken"));
+
+// Use previously requested tokens fetched from somewhere. A database for example.
+if ($accessToken) {
+    $session->setAccessToken($accessToken);
+    $session->setRefreshToken($refreshToken);
+} else {
+    // Or request a new access token
+    $session->refreshAccessToken($refreshToken);
+}
+
+$options = [
+    'auto_refresh' => true,
+];
+
+
+
+$api = new SpotifyWebAPI\SpotifyWebAPI($options, $session);
+
+$volume = $api->getMyCurrentPlaybackInfo()->device->volume_percent;
+
+
+echo $volume;
+// var_dump($api->changeVolume(['volume_percent' => $volume,]));
