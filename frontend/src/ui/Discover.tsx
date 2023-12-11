dba816807cae47f8233b1cb97977c4cc482cec50
Federico Tensi <federicotensi@gmail.com>
2023-12-11 22:09:49 +0100

    fetching playlist image from spotify api

--- frontend/src/ui/Discover.tsx
+++ frontend/src/ui/Discover.tsx
@@ -1,11 +1,12 @@
 import { ArrowRight } from "lucide-react";
 import { Card, CardDescription } from "@/components/ui/card";
 import { genres } from "@/helpers/arrayList";
-import { genre } from "@/helpers/types";
+import { genre, getPlaylistImageResponse } from "@/helpers/types";
 
 import { useNavigate } from "react-router-dom";
 import { motion, useAnimation, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { API_URL } from "@/helpers/endpoints";
 
 const Title = () => {
 	return (
@@ -52,25 +53,44 @@ export const GenreCard = ({ title, description, imageUrl, trackUri, width }: gen
 		}
 	}, [isInView]);
 
+	const [processedImageUrl, setProcessedImageUrl] = useState<string>("");
+
+	useEffect(() => {
+		if (trackUri === "") return;
+		if (imageUrl !== undefined) return setProcessedImageUrl(imageUrl);
+
+		const parsedTrackUri = trackUri.split("PL=")[1];
+
+		const url = `${API_URL}/getPlaylistImage.php?uri=${parsedTrackUri}`;
+		const request = fetch(url);
+		request
+			.then((response) => response.json())
+			.then((data: getPlaylistImageResponse[]) => {
+				setProcessedImageUrl(data[0].url);
+				// setImageUrl(data.images[0].url);
+			});
+	}, [trackUri, imageUrl, !isInView]);
+
 	return (
 		<motion.div ref={cardRef} initial={{ opacity: 0 }} animate={controls} transition={{ duration: 0.6 }}>
 			<Card
 				onClick={() => navigate(`/discover/${trackUri}/${encodedTitle}`)}
-				className={`cursor-pointer ${width}  h-[15rem] relative overflow-clip border-0 bg-neutral-800 rounded-3xl flex-none `}
+				className={`cursor-pointer ${width}   relative overflow-clip border-0 bg-neutral-800 rounded-3xl aspect-square `}
 			>
-				<div className="absolute flex radial-bg  backdrop-blur-[2px] text-white w-full h-48 font-bold justify-center items-center rounded-b-3xl">
-					<div className="text-lg ">{title}</div>
+				<div className="absolute z-20 flex top-0 backdrop-blur-[2px] text-white w-full font-semibold justify-center  bg-neutral-900 bg-opacity-60">
+					<div className="p-2 text-lg ">{title}</div>
 				</div>
+
 				<div className="relative flex justify-center">
 					<img
-						src={imageUrl}
+						src={processedImageUrl}
 						alt=""
 						className="absolute z-10 self-center w-48 opacity-80 rounded-2xl snap-center aspect-square"
 					/>
-					<img src={imageUrl} alt="" className="z-0 self-center w-full blur-xl aspect-square " />
+					<img src={processedImageUrl} alt="" className="z-0 self-center w-full blur-xl aspect-square " />
 				</div>
 
-				<div className="absolute bottom-0 z-20 w-full p-3 text-left bg-opacity-50 bg-neutral-900 ">
+				<div className="absolute bottom-0 z-30 w-full p-3 text-left bg-opacity-50 bg-neutral-900 backdrop-blur-[2px]">
 					<CardDescription className="pt-1 text-sm text-white">{description}</CardDescription>
 				</div>
 			</Card>
