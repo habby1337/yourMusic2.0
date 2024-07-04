e32f2ed2ed0c65a84e3b3b90f169e771f247af73
Federico Tensi <federicotensi@gmail.com>
2024-07-04 12:45:35 +0200

    chore: Sentry error monitoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from '@sentry/react'
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromChildren, matchRoutes, RouterProvider, useLocation, useNavigationType } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
@@ -10,8 +11,30 @@ import DiscoverGenre from "./ui/DicoverGenre.tsx";
 import { AuthorizeMe } from "./ui/AuthorizeMe.tsx";
 import { SecretPanel } from "./ui/SecretPanel.tsx";
 
+
 const queryClient = new QueryClient();
-const router = createBrowserRouter([
+
+Sentry.init({
+	dsn: import.meta.env.VITE_SENTRY_DSN, 
+	integrations: [
+		Sentry.reactRouterV6BrowserTracingIntegration({
+			useEffect: React.useEffect,
+			useLocation,
+			useNavigationType,
+			createRoutesFromChildren,
+			matchRoutes
+		}),
+		Sentry.replayIntegration(),
+	],
+	tracesSampleRate: 1.0,
+	tracePropagationTargets: ["localhost", /^https:\/\/sp.tensi\.dev\/.*/, / * /],
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0
+})
+
+const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouter(createBrowserRouter)
+
+const router = sentryCreateBrowserRouter([
 	{ path: "/", Component: App },
 	{ path: "/discover/:genre?/:title?", Component: DiscoverGenre },
 	{ path: "/authorizeMe", Component: AuthorizeMe },
